2783faa9613c43897e87838cde90917ec4ff4eec
cer2891 <69424533+cer2891@users.noreply.github.com>
2021-10-10 10:02:41 +0800

    Update settings.py
    delete SECRET_KEY

--- magazin_rostokk/settings.py
+++ magazin_rostokk/settings.py
@@ -20,7 +20,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x%vm2g@v_p+60$qvytda8%2-@-y(vfsm8@0h6z%&rosyp6wo9='
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
